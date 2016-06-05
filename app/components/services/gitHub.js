858ac6ab700501b2610179e8edbf1b915cb578e6
Vinod Satpute <Vinod Satpute>
2016-06-05 10:30:06 +0530

    FF Commit

--- app/components/services/gitHub.js
+++ app/components/services/gitHub.js
@@ -2,23 +2,18 @@
   angular.module('ghViewer.gitHubServices',[])
 
   .factory('gitHubService',function($http,apiUrl,projectUrl){
-
     var getUser = function(username) {
       return $http.get(apiUrl + username);
     };
-
     var getRepo = function(username) {
       return $http.get(apiUrl + username + "/repos");
     };
-
     var getContributers = function(username,projectname) {
       return $http.get(projectUrl + username + "/" + projectname + "/contributors");
     };
-
     var getProjectDetails = function(username,projectname) {
       return $http.get(projectUrl + username + "/" + projectname);
     }
-
     return {
       getUser:getUser,
       getRepo:getRepo,
